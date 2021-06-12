b050837025634dabc8c9c95bbfc9bb75f56eb36f
sokmen4083 <sokmen4083@gmail.com>
2021-06-13 01:25:22 +0200

    Updates

---
+++ precache-manifest.8c84ecab28b673c621fd2a8effff7052.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449c1daee6a77702d38268beb3a17c97",
+    "url": "/pref-last/index.html"
+  },
+  {
+    "revision": "c75a66357777c985bb50",
+    "url": "/pref-last/static/css/2.427a1f5a.chunk.css"
+  },
+  {
+    "revision": "6f8b4fc652b667367f6d",
+    "url": "/pref-last/static/css/main.ebd57477.chunk.css"
+  },
+  {
+    "revision": "c75a66357777c985bb50",
+    "url": "/pref-last/static/js/2.5117f1cc.chunk.js"
+  },
+  {
+    "revision": "166b61fd4bd540afed78fd9d5c882b00",
+    "url": "/pref-last/static/js/2.5117f1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8b4fc652b667367f6d",
+    "url": "/pref-last/static/js/main.efd83c38.chunk.js"
+  },
+  {
+    "revision": "08eab7ee3eaab9acaca0",
+    "url": "/pref-last/static/js/runtime-main.3a043f5e.js"
+  },
+  {
+    "revision": "18c3888f364246a853eb25e459360731",
+    "url": "/pref-last/static/media/change.18c3888f.svg"
+  }
+]);
