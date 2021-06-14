860cb158ae2ff04252c1efeab0f068c1d71e4440
sokmen4083 <sokmen4083@gmail.com>
2021-06-14 03:28:19 +0200

    select Country added

--- src/App.js
+++ src/App.js
@@ -84,12 +84,11 @@ function App() {
                  <Alert variant="success">
                     <p>{t('info')}</p>
                 </Alert>
-                    <Route path='/PrefDocuments'  component={PrefDocuments}/>
                     <Route path='/FamilyUnion' component={Family}/>                   
                     <Route path='/fileCombination' component={FileCombination}/>
                     <Route path='/CantonChange' component={CantonChange}/>
                     <Route path='/CampChange' component={CampChange}/>
-                    <Route path='/'   component={PrefDocuments}/>
+                    <Route path='/' exact={true}  component={PrefDocuments}/>
             </Col>
           </Row>
         </Container>
