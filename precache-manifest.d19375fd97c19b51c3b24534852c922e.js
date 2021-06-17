4adef9b5498e138d783c6e14be42e2ea1bf647a9
sokmen4083 <sokmen4083@gmail.com>
2021-06-17 22:43:39 +0200

    Updates

---
+++ precache-manifest.d19375fd97c19b51c3b24534852c922e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6402cfd7b5ea6b3788c7001110358ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80dcafc897137bc4513",
+    "url": "/static/css/2.427a1f5a.chunk.css"
+  },
+  {
+    "revision": "ce65b1ceb9d07a01516c",
+    "url": "/static/css/main.5ae9896a.chunk.css"
+  },
+  {
+    "revision": "a80dcafc897137bc4513",
+    "url": "/static/js/2.774f5544.chunk.js"
+  },
+  {
+    "revision": "166b61fd4bd540afed78fd9d5c882b00",
+    "url": "/static/js/2.774f5544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce65b1ceb9d07a01516c",
+    "url": "/static/js/main.0a7e4ee7.chunk.js"
+  },
+  {
+    "revision": "6f2a7152b3a4473f2533",
+    "url": "/static/js/runtime-main.dbdb7264.js"
+  },
+  {
+    "revision": "18c3888f364246a853eb25e459360731",
+    "url": "/static/media/change.18c3888f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pref-last/precache-manifest.74d60138cf3a62e071fa3774d9c45eaa.js"
+  "/precache-manifest.d19375fd97c19b51c3b24534852c922e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pref-last/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
