3599ecfa68bdc81d0537605699934dd977daa607
sokmen4083 <sokmen4083@gmail.com>
2021-08-07 09:23:09 +0200

    design has done

--- src/App.js
+++ src/App.js
@@ -84,7 +84,6 @@ function App() {
                  <Alert variant="success">
                     <p>{t('info')}</p>
                 </Alert>
-                    <Route path='/' exact={true}  component={PrefDocuments}/>
                     <Route path='/pref-last'  component={PrefDocuments}/>
                     <Route path='/FamilyUnion' component={Family}/>                   
                     <Route path='/fileCombination' component={FileCombination}/>
